0907ae7920cae202a51e2242eab5446b78347e90
nrb2002 <tshibasubaron@yahoo.fr>
2025-07-06 21:50:38 +0100

    New updates

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,8 @@
-// HEADER SCRIPTS
+// HEADER AND NAVIGATION SCRIPTS
+const navLinks = document.querySelector('#nava-bar');
 const navButton = document.querySelector('#ham-btn');
 
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
+    navLinks.classList.toggle('show'); 
 });
--- week01/scripts/header.js
+++ week01/scripts/header.js
@@ -1,6 +1,8 @@
 // HEADER SCRIPTS
 const navButton = document.querySelector('#ham-btn');
+const navBar = document.querySelector('#nav-bar');
 
 navButton.addEventListener('click', () => {
-    navButton.classList.toggle('show');
+  navButton.classList.toggle('show'); 
+  navBar.classList.toggle('show');  
 });
