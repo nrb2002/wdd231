154f89774a9f8c7e199249b6032893632ef631f6
nrb2002 <tshibasubaron@yahoo.fr>
2025-08-02 00:57:35 +0100

    New update

--- garage/scripts/getdates.js
+++
@@ -1,8 +0,0 @@
-// Get current year
-let currentyear = document.getElementById ("currentyear");
-const date = new Date();
-currentyear.innerHTML = date.getFullYear();
-
-// Last modified
-let lastUpdate = document.getElementById("lastModified");
-lastUpdate.innerHTML = document.lastModified
